e53071056bd0e1cc577ae9be94e4e497d4e68cdd
manoj johnson <manojjohn1838@gmail.com>
2022-02-07 11:51:57 +0530

    update dao,service and validator

--- src/main/java/com/chainsys/demoapp/dao/UserDAO.java
+++ src/main/java/com/chainsys/demoapp/dao/UserDAO.java
@@ -1,6 +1,7 @@
 package com.chainsys.demoapp.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,15 +13,33 @@ import com.chainsys.demoapp.util.ConnectionUtil;
 
 
 public class UserDAO {
+	
+	/**
+	 * make it as singlton 
+	 */
+	public static UserDAO userDao;
+	private UserDAO()
+	{
+		
+	}
+	public static UserDAO getInstance()
+	{
+		if(userDao==null)
+		{
+			userDao=new UserDAO();
+		}
+		return userDao;
+		
+	}
 
 	/**
 	 * @param user object
 	 * @return boolean true if user details inserted
 	 */
-	public boolean userInsert(User user) {
+	public boolean insertUser(User user) {
 		Connection con = ConnectionUtil.getDbConnect();
 
-		String query = "insert into UserSample123 values('" + user.getName() + "','" + user.getEmail() + "','"
+		String query = "insert into UserSample123 (name,email,password) values('" + user.getName() + "','" + user.getEmail() + "','"
 				+ user.getPassword() + "')";
 		boolean flag = false;
 		Statement stmt = null;
@@ -36,12 +55,37 @@ public class UserDAO {
 
 		return flag;
 	}
+	/**
+	 * @param user
+	 * this method is to update user details in the table
+	 * @return boolean true if value updated
+	 */
+	public boolean updateUser(User user) {
+		String insertQuery = "update table usersample123 name=?,password=? where email=?";
+		
+		Connection con = ConnectionUtil.getDbConnect();
+		boolean result = false;
+		PreparedStatement pstmt = null;
+		try {
 
+			pstmt = con.prepareStatement(insertQuery);
+			pstmt.setString(1, user.getName());
+			pstmt.setString(2, user.getEmail());
+			pstmt.executeUpdate();
+			
+
+		} catch (SQLException e) {
+			// catch the exception and get that message
+			e.printStackTrace();
+			
+		}
+		return result;
+	}
 	
 	/**
-	 * @return list of user objects fetched data from the table
+	 * @return list of user objects fetched from the table
 	 */
-	public List<User> showAll() {
+	public List<User> findAllUsers() {
 		List<User> userList = new ArrayList<User>();
 
 		String query = "select * from userSample123";
@@ -64,6 +108,54 @@ public class UserDAO {
 		}
 
 		return userList;
+
+	}
+	/**
+	 * @param user
+	 * this method is to fetch user id from the table
+	 * @return int user id 
+	 */
+	public int findUserId(User user) {
+		String findUserID = "select id from usersample123 where email= '" + user.getEmail() + "'";
+		Connection con = ConnectionUtil.getDbConnect();
+		Statement stmt;
+		int userId = 0;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(findUserID);
+			if (rs.next()) {
+				userId = rs.getInt(1);
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return userId;
+
+	}
+	/**
+	 * @param int userId
+	 * this method is to fetch user details from the table
+	 * @return User Object
+	 */
+	public User findUser(int userId) {
+		String findUser = "select name,email,password from usersample123 where userID= '" + userId + "'";
+		Connection con = ConnectionUtil.getDbConnect();
+		Statement stmt;
+		User user = null;
+		try {
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(findUser);
+			if (rs.next()) {
+				user = new User(rs.getString(1), rs.getString(2), rs.getString(3));
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+		return user;
 
 	}
 
--- src/main/java/com/chainsys/demoapp/service/UserService.java
+++ src/main/java/com/chainsys/demoapp/service/UserService.java
@@ -1,5 +1,28 @@
 package com.chainsys.demoapp.service;
 
+import java.util.List;
+
+import com.chainsys.demoapp.dao.UserDAO;
+import com.chainsys.demoapp.model.User;
+
 public class UserService {
 
+	public List<User> findAllUsers() {
+		UserDAO userDao = UserDAO.getInstance();
+		return userDao.findAllUsers();
+	}
+
+	public boolean userInsert(User user) {
+		UserDAO userDao = UserDAO.getInstance();
+		return userDao.insertUser(user);
+	}
+
+	public boolean updateUser(User user) {
+		UserDAO userDao = UserDAO.getInstance();
+		return userDao.updateUser(user);
+	}
+	public User findUser(int userId) {
+		UserDAO userDao = UserDAO.getInstance();
+		return userDao.findUser(userId);
+	}
 }
